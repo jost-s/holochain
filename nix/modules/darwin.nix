2aebee39ae31573e341e2be3e6184ecf9a9fa1ad
Jost Schulte <jost.schulte@protonmail.com>
2024-05-26 08:32:30 -0600

    try with sdk 11

--- nix/modules/darwin.nix
+++ nix/modules/darwin.nix
@@ -11,9 +11,10 @@
       legacyPackages.apple_sdk' = lib.attrsets.optionalAttrs pkgs.stdenv.isDarwin
         (
           # aarch64 only uses 11.0 and x86_64 mixes them
-          if system == "x86_64-darwin"
-          then pkgs.darwin.apple_sdk_10_12
-          else pkgs.darwin.apple_sdk_11_0
+          # if system == "x86_64-darwin"
+          # then pkgs.darwin.apple_sdk_10_12
+          # else pkgs.darwin.apple_sdk_11_0
+          pkgs.darwin.apple_sdk_11_0
         );
     };
 }
