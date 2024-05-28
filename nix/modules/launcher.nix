b4fdba79ca84ba74413610d93b42aa445c109fd9
Jost Schulte <jost.schulte@protonmail.com>
2024-05-28 16:44:27 -0600

    successful build

--- nix/modules/launcher.nix
+++ nix/modules/launcher.nix
@@ -5,7 +5,7 @@
     let
       rustToolchain = config.rustHelper.mkRust {
         track = "stable";
-        version = "1.71.1";
+        version = "1.77.2";
       };
       craneLib = inputs.crane.lib.${system}.overrideToolchain rustToolchain;
 
