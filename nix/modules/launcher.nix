acb44bfd2456bf14b722d278226efb6981216265
Jost Schulte <jost.schulte@protonmail.com>
2024-05-30 15:02:55 -0600

    remove prefix phase

--- nix/modules/launcher.nix
+++ nix/modules/launcher.nix
@@ -74,21 +74,6 @@
             pkgs.overrideSDK pkgs.stdenv "11.0"
           else
             pkgs.stdenv;
-
-        nativeBuildInputs = commonArgs.nativeBuildInputs ++ [
-          pkgs.makeBinaryWrapper
-        ];
-
-        preFixup = ''
-          gappsWrapperArgs+=(
-            --set WEBKIT_DISABLE_COMPOSITING_MODE 1
-          )
-
-          # without this the DevTools will just display an unparsed HTML file (see https://github.com/tauri-apps/tauri/issues/5711#issuecomment-1336409601)
-          gappsWrapperArgs+=(
-            --prefix XDG_DATA_DIRS : "${pkgs.shared-mime-info}/share"
-          )
-        '';
       });
 
     in
